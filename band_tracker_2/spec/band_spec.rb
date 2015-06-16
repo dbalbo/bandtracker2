78e82155315ddc8028bec39829a3fbe1301595ad
debbie <debbalbo@hotmail.com>
2015-06-16 15:58:59 -0700

    beggining of ar bandtracker

--- band_tracker_2/spec/band_spec.rb
+++ band_tracker_2/spec/band_spec.rb
@@ -1,6 +1,11 @@
 
 require('spec_helper')
 
-describe(Band) do
-  it { should have_and_belong_to_many(:venues)}
-end
+  describe(Band) do
+    it { should have_and_belong_to_many(:venues) }
+  end
+
+  it('validates the presence of a band name') do
+    band = Band.new({:name => ''})
+    expect(band.save).to(eq(false))
+  end
